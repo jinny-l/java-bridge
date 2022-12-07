ba22f42e0fa54d67e77feeb5097a2525ced3d455
jinny-l <jinny.l.dev@gmail.com>
2022-12-07 16:28:06 +0900

    feat: 다리 길이 입력 기능 구현

---
+++ src/main/java/bridge/view/InputView.java
@@ -0,0 +1,47 @@
+package bridge.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 사용자로부터 입력을 받는 역할을 한다.
+ */
+public class InputView {
+
+    /**
+     * 다리의 길이를 입력받는다.
+     */
+    public static int readBridgeSize() {
+        System.out.println("다리 길이를 입력해주세요.");
+        try {
+            return Integer.parseInt(readLine());
+        } catch (NumberFormatException ne) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    /**
+     * 사용자가 이동할 칸을 입력받는다.
+     */
+    public static String readMoving() {
+        return null;
+    }
+
+    /**
+     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
+     */
+    public static String readGameCommand() {
+        return null;
+    }
+
+    private static String readLine() {
+        String input = Console.readLine().trim();
+        validateHasInput(input);
+        return input;
+    }
+
+    private static void validateHasInput(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
