983aebea29b34d474f8be6dc9b3bd86717ffebc0
jinny-l <jinny.l.dev@gmail.com>
2022-12-07 17:58:24 +0900

    feat: 다리 건너기 결과 출력 기능 구현
    * OutputView.java
    OutputView.convertDataToBridge: 데이터를 다리 출력 형식에 맞게 변환하는 기능(다리 중간 부분)
    OutputView.printBridge: 다리 시작/끝 부분을 다리 중간 부분과 합쳐 출력 형식에 맞게 변한하는 기능
    OutputView.printMap: 다리를 출력 형식에 맞게 출력하는 기능

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,10 +1,19 @@
 package bridge.view;
 
+import java.util.List;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
 
+    private static final String BRIDGE_START_DELIMITER = "[ ";
+    private static final String BRIDGE_END_DELIMITER = " ]";
+    private static final String BLOCK_DELIMITER = " | ";
+
+    /**
+     * 게임 시작 시 출력하는 메서드
+     */
     public static void printStart() {
         System.out.println("다리 건너기 게임을 시작합니다.\n");
     }
@@ -14,7 +23,28 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public static void printMap(List<String> upBridgeResult, List<String> downBridgeResult) {
+        System.out.println(printBridge(upBridgeResult));
+        System.out.println(printBridge(downBridgeResult));
+        System.out.println();
+    }
+
+    private static String printBridge(List<String> bridgeResult) {
+        return BRIDGE_START_DELIMITER
+                + convertDataToBridge(bridgeResult)
+                + BRIDGE_END_DELIMITER;
+    }
+
+    private static String convertDataToBridge(List<String> bridgeResult) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < bridgeResult.size(); i++) {
+            stringBuilder.append(bridgeResult.get(i));
+            if (i == bridgeResult.size() - 1) {
+                break;
+            }
+            stringBuilder.append(BLOCK_DELIMITER);
+        }
+        return stringBuilder.toString();
     }
 
     /**
